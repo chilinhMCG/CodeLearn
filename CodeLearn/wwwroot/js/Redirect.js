b37e02412ece02810280bd6cca853b57a1ca1a24
kebxkhw <tuankhiem2405@gmail.com>
2021-09-19 15:48:20 +0700

    course edit page

--- CodeLearn/wwwroot/js/Redirect.js
+++ CodeLearn/wwwroot/js/Redirect.js
@@ -1,3 +1,7 @@
 ﻿function RedirectToCourseType() {
     window.location.href = "/admin/course-type-manager";
 }
+
+function RedirectToCourse() {
+    window.location.href = "/admin/course-manager";
+}
