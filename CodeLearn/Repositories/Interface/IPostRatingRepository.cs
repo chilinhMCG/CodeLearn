5bce42d818739325c8579a1023cbe892d88d6382
CMB <thaichibao@gmail.com>
2021-11-03 08:07:31 +0700

    Add PostRatingRepository

---
+++ CodeLearn/Repositories/Interface/IPostRatingRepository.cs
@@ -0,0 +1,28 @@
+﻿using CodeLearn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeLearn.Repositories.Interface
+{
+    public interface IPostRatingRepository
+    {
+        /// <summary>
+        /// <para>If user has not rate the post, then that post rating value will be 0.</para> 
+        /// </summary>
+        public Task<PostRating> GetAsync(Guid userId, Guid postId);
+
+        /// <summary>
+        /// <para>Get a range of post ratings of a given user.</para> 
+        /// <para>If user has not rate a post, then that post rating value will be 0.</para> 
+        /// </summary>
+        public Task<IList<PostRating>> GetRangeAsync(Guid userId, IEnumerable<Guid> postIds);
+
+        public Task AddAsync(PostRating postRating);
+
+        public Task UpdateAsync(PostRating postRating);
+
+        public Task RemoveAsync(PostRating postRating);
+    }
+}
---
+++ CodeLearn/Repositories/PostRatingRepository.cs
@@ -0,0 +1,79 @@
+﻿using CodeLearn.Data;
+using CodeLearn.Models;
+using CodeLearn.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeLearn.Repositories
+{
+    public class PostRatingRepository : IPostRatingRepository
+    {
+        private readonly IDbContextFactory<ApplicationDBContext> _dbContextFactory;
+
+        public PostRatingRepository(IDbContextFactory<ApplicationDBContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<PostRating> GetAsync(Guid userId, Guid postId)
+        {
+            using var context = _dbContextFactory.CreateDbContext();
+            var postRating = await context.PostRatings.FindAsync(userId, postId);
+            return postRating ?? new PostRating() { UserId = userId, PostId = postId, Value = 0 };
+        }
+
+        public async Task<IList<PostRating>> GetRangeAsync(Guid userId, IEnumerable<Guid> postIds)
+        {
+            using var context = _dbContextFactory.CreateDbContext();
+
+            var postRatings = new List<PostRating>(postIds.Count());
+
+            foreach (Guid postId in postIds)
+            {
+                var rating = await context.PostRatings.FindAsync(userId, postId);
+
+                if (rating == null)
+                {
+                    rating = new PostRating()
+                    {
+                        PostId = postId,
+                        UserId = userId,
+                        Value = 0,
+                    };
+                }
+
+                postRatings.Add(rating);
+            }
+
+            return postRatings;
+        }
+
+        public async Task AddAsync(PostRating postRating)
+        {
+            using var context = _dbContextFactory.CreateDbContext();
+            await context.PostRatings.AddAsync(postRating);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(PostRating updatedPostRating)
+        {
+            using var context = _dbContextFactory.CreateDbContext();
+            PostRating postRating = await context.PostRatings.FindAsync(updatedPostRating.UserId, updatedPostRating.PostId);
+
+            postRating.Value = updatedPostRating.Value;
+            await context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(PostRating postRating)
+        {
+            using var context = _dbContextFactory.CreateDbContext();
+            context.PostRatings.Attach(postRating);
+            context.PostRatings.Remove(postRating);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
