e72702a1eb77e4ee218d6a21b4ff9d1c7ee71295
CMB <thaichibao@gmail.com>
2021-11-10 14:17:21 +0700

    Add Pagination Utils

---
+++ CodeLearn/Data/PaginationUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace CodeLearn.Data
+{
+    //paging is one-indexed
+    public static class PaginationUtils
+    {
+        public static int GetPageCount(int pageSize, int allItemsCount)
+        {
+            int pageCount = allItemsCount / pageSize;
+
+            if (allItemsCount - (pageSize * pageCount) > 0)
+                pageCount++;
+
+            return pageCount;
+        }
+
+        public static IQueryable<T> TakeFromPage<T>(this IQueryable<T> query, int pageNumber, int pagesize)
+        {
+            return query.Skip(pagesize * (pageNumber - 1))
+                        .Take(pagesize);
+        }
+    }
+}
