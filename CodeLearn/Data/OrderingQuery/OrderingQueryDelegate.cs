803ccba65abfb6cba05bd3fa996ddefcafee07a5
CMB <thaichibao@gmail.com>
2021-11-10 14:20:18 +0700

    Add OrderingQuery classes and delegate, allow client to order content of a query

---
+++ CodeLearn/Data/OrderingQuery/OrderingQueryDelegate.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeLearn.Data.OrderingQuery
+{
+    public delegate OrderedOrderingQuery<T> OrderingQueryDelegate<T>(OrderingQuery<T> orderingOptions);
+}
