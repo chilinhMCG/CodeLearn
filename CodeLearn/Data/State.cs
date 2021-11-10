a5bf5bf0481e6c5cad3707b17300891149f8498f
CMB <thaichibao@gmail.com>
2021-11-10 14:15:17 +0700

    Add State

---
+++ CodeLearn/Data/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeLearn.Data
+{
+    public enum State
+    {
+        Init, NotLoaded, Loading, NotFound, Complete
+    }
+}
