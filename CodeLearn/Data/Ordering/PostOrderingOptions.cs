73e5500404b1adc7736212f5c666e34c2d945a8a
Thái Chí Bảo <comaybayy@gmail.com>
2021-12-01 17:44:42 +0700

    Add PostOrderingOption enum

---
+++ CodeLearn/Data/Ordering/PostOrderingOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeLearn.Data.Ordering
+{
+    public enum PostOrderingOption
+    {
+        Ratings, Comments, Relevance, OverallRating, DateCreated
+    }
+
+
+}
